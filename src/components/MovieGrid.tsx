2cb5a2147c068618e9287bc0862c414f1de6b297
Konika Nahar <konika.nahar@gmail.com>
2025-03-20 17:33:56 -0500

    implemented infinite queries

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,39 +1,56 @@
 import useMovies from "@/hooks/useMovies";
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
 import MovieCard from "./MovieCard";
 import MovieCardContainer from "./MovieCardContainer";
-import { Genre } from "@/hooks/useGenres";
 import MovieCardSkeleton from "./MovieCardSkeleton";
 import { MovieQuery } from "@/App";
+import React from "react";
 
 interface Props {
   movieQuery: MovieQuery;
 }
 
 const GameGrid = ({ movieQuery }: Props) => {
-  const { data: movies, error, isLoading } = useMovies(movieQuery);
+  const {
+    data: movies,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useMovies(movieQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return <Text>{error.message}</Text>;
 
   return (
-    <SimpleGrid
-      columns={{ base: 1, sm: 2, md: 4, lg: 5, xl: 6 }}
-      gap={{ sm: 5, md: 5, lg: 5, xl: 6 }}
-      padding="10px"
-    >
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <MovieCardContainer key={skeleton}>
-            <MovieCardSkeleton />
-          </MovieCardContainer>
+    <Box padding="10px">
+      <SimpleGrid
+        columns={{ base: 1, sm: 2, md: 4, lg: 5, xl: 6 }}
+        gap={{ sm: 5, md: 5, lg: 5, xl: 6 }}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <MovieCardContainer key={skeleton}>
+              <MovieCardSkeleton />
+            </MovieCardContainer>
+          ))}
+        {movies?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results?.map((movie) => (
+              <MovieCardContainer key={movie.id}>
+                <MovieCard movie={movie} />
+              </MovieCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {movies?.results?.map((movie) => (
-        <MovieCardContainer key={movie.id}>
-          <MovieCard movie={movie} />
-        </MovieCardContainer>
-      ))}
-    </SimpleGrid>
+      </SimpleGrid>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} marginY="5">
+          {isFetchingNextPage ? "Loading..." : "Load More"}
+        </Button>
+      )}
+    </Box>
   );
 };
 
